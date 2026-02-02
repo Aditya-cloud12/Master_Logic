6fb1a79c2cd538752447dff229dd510cad3afbbc
Aditya-cloud12 <adityaraj@localhost>
2026-02-02 19:21:02 +0530

    Date 2 Feb

---
+++ LogicCode/src/Master_Logic/WeekdayOrWeekend.java
@@ -0,0 +1,24 @@
+package Master_Logic;
+
+import java.util.Scanner;
+
+public class WeekdayOrWeekend {
+
+	    public static void main(String[] args) {
+
+	     Scanner sc = new Scanner(System.in);
+	     
+	     System.out.println("Enter Day Number :");
+	     int day = sc.nextInt();
+	     
+	        if (day >= 1 && day <= 5) {
+	            System.out.println("Weekday");
+	        } else if (day == 6 || day == 7) {
+	            System.out.println("Weekend");
+	        } else {
+	            System.out.println("Invalid day number");
+	        }
+	    }
+	}
+
+
