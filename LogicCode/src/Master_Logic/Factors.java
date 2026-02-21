a46f6150366798276f13842cc0019000a4a2d13b
Aditya-cloud12 <adityaofficial2025@gmail.com>
2026-02-21 19:50:02 +0530

    Date 21 Feb

---
+++ LogicCode/src/Master_Logic/Factors.java
@@ -0,0 +1,20 @@
+package Master_Logic;
+
+import java.util.Scanner;
+
+public class Factors {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter number: ");
+		int n = sc.nextInt();
+
+		System.out.println("Factors are:");
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
