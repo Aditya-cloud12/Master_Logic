73ddb4822ade5f16e2410bd28fe2f18fbcb49770
Aditya-cloud12 <adityaofficial2025@gmail.com>
2026-02-22 20:27:25 +0530

    Date 22 feb

---
+++ LogicCode/src/Master_Logic/SumOfFactors.java
@@ -0,0 +1,22 @@
+package Master_Logic;
+
+import java.util.Scanner;
+
+public class SumOfFactors {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				sum += i;
+			}
+		}
+
+		System.out.println("Sum of factors = " + sum);
+	}
+}
