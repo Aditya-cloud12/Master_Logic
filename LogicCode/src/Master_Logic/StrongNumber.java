f2c56074ac2005c45b0919853a07be472fbd0269
Aditya-cloud12 <adityaofficial2025@gmail.com>
2026-02-24 21:07:06 +0530

    Date 24 feb

--- LogicCode/src/Master_Logic/StrongNumber.java
+++ LogicCode/src/Master_Logic/StrongNumber.java
@@ -1,10 +1,35 @@
 package Master_Logic;
 
+import java.util.Scanner;
+
 public class StrongNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("Enter number: ");
+		int n = sc.nextInt();
+
+		int original = n;
+		int sum = 0;
+
+		while (n != 0) {
+			int digit = n % 10;
+
+			int fact = 1;
+			for (int i = 1; i <= digit; i++) {
+				fact *= i;
+			}
+
+			sum += fact;
+			n /= 10;
+		}
+
+		if (sum == original) {
+			System.out.println("Strong Number");
+		} else {
+			System.out.println("Not a Strong Number");
+		}
 	}
 
 }
